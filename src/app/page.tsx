bebc3ded2015448e6eeed031d0e62b3a66ede891
sandipopovic988 <laflamablanca988@hotmail.com>
2025-02-01 14:18:26 +0100

    Page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,13 @@ export default function Home() {
         className="object-cover brightness-75"
       />
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-white">Bebooo!</h1>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-4xl font-bold text-white">Bebooo!</h1>
+          <div className="flex flex-col justify-center w-full">
+            <p className="text-center">Mali korak za nas,</p>
+            <p className="text-center">Veliki za Svijetli Raj</p>
+          </div>
+        </div>
       </div>
     </div>
   );
